7884df684ad47b74b1bbf13f9a891e6811fea5e5
Paulo César <paulofilhobarroso@gmail.com>
2022-06-21 15:47:04 -0300

    Header + Home + Balance UI init

---
+++ src/components/Balance/index.js
@@ -0,0 +1,21 @@
+import { 
+    View,
+    Text,
+    StyleSheet
+} from 'react-native'
+import React from 'react'
+
+export default function Balance() {
+  return (
+    <View style={styles.container}>
+      <Text>Test Component</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+    container: {
+
+    }
+
+})
